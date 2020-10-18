d7234b82f6907c408f1c3b6c3b539622e720521a
jahnvi <jbdalwadi01@gmail.com>
2020-10-18 14:14:23 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports={
-    mongoURI:'mongodb+srv://jahnvi:jahnvi001@cluster0-0uzok.mongodb.net/job?retryWrites=true&w=majority',
+    mongoURI:'',
    JWT_SECRET:'secretkey',
-   EMAIL_API:'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',
-   EMAIL_TO:'jbdalwadi01@gmail.com',
+   EMAIL_API:'',
+   EMAIL_TO:'',
    JWT_RESET_PASSWORD:'secret',
    EMAIL_FROM:'noreply@gmail.com',
    JWT_ACCOUNT_ACTIVATION:'activate',
@@ -10,7 +10,7 @@ module.exports={
    JWT_INTERVIEWER_ACTIVATION:'interviewer',
    ADMIN_USERNAME:'RS_TechHire',
    ADMIN_PASSWORD:'RS@ashk',
-   EMAIL:' jbdalwadi01@gmail.com',
+   EMAIL:'',
    URL:'http://localhost:3000'
 
 }
